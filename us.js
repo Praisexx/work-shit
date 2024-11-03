287ad12bc6feb1954ebd5961055f6d6c87df8ee2
Praisexx <141040038+Praisexx@users.noreply.github.com>
2024-11-03 12:48:43 +0000

    hehehehe

--- us.js
+++ us.js
@@ -7,3 +7,10 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const hammenu =document.querySelector('.ham-menu');
+const navlinks =document.querySelector('.nav-links');
+hammenu.addEventListener('click',() => {
+    hammenu.classList.toggle("active");
+    navlinks.classList.toggle("active");
+});
